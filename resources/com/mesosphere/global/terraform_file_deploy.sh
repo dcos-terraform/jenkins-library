2a64c7e3b27043002c6ace9e947020f57e295988
Miguel Bernadin <Miguel.Bernadin@gmail.com>
2019-05-16 17:55:02 +0200

    cicd: enabling debug

--- resources/com/mesosphere/global/terraform_file_deploy.sh
+++ resources/com/mesosphere/global/terraform_file_deploy.sh
@@ -10,6 +10,8 @@ then
   echo ${DCOS_CONFIG}
   git clone --single-branch --branch terraform_file_method_ci_deploy2 https://github.com/dcos-terraform/jenkins-library.git
   cp -fr jenkins-library/resources/com/mesosphere/global/terraform-file-dcos-terraform-test-examples/$2-$3/ "${TMP_DCOS_TERRAFORM}" || exit 1
+  ls -l "${TMP_DCOS_TERRAFORM}" # debug purpose
+	env # debug
 else
   eval "$(cat ci-deploy.state)"
 fi
