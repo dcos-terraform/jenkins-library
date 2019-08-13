7aa76f15aa75ca8d523650a77cf6668a68a51b34
Oleksandr Bielov1 <Oleksandr_Bielov1@epam.com>
2019-08-13 15:05:48 +0300

    5405: run SRE test scenario for azure

--- resources/com/mesosphere/global/terraform-file-dcos-terraform-test-examples/azurerm-0.2.x/main.tf
+++ resources/com/mesosphere/global/terraform-file-dcos-terraform-test-examples/azurerm-0.2.x/main.tf
@@ -3,42 +3,74 @@ provider "azurerm" {}
 data "http" "whatismyip" {
   url = "http://whatismyip.akamai.com/"
 }
-
 resource "random_id" "cluster_name" {
   prefix      = "dcos-terraform-"
   byte_length = 4
 }
 
+locals {
+  cluster_name     = "${random_id.cluster_name.hex}"
+  location         = "West US" 
+  dcos_winagent_os = "windows_1809"
+  vm_size          = "Standard_D2s_v3"
+}
 module "dcos" {
   source  = "dcos-terraform/dcos/azurerm"
   version = "~> 0.2.0"
 
   dcos_instance_os    = "${var.dcos_instance_os}"
-  cluster_name        = "${random_id.cluster_name.hex}"
-  ssh_public_key_file = "./ssh-key.pub"
+  ssh_public_key_file = "${var.ssh_public_key_file}"
   admin_ips           = ["${data.http.whatismyip.body}/32"]
-  location            = "West US"
+  location            = "${local.location}"
 
   num_masters        = "${var.num_masters}"
   num_private_agents = "${var.num_private_agents}"
   num_public_agents  = "${var.num_public_agents}"
 
+  ansible_bundled_container = "mesosphere/dcos-ansible-bundle:feature-windows-support-039d79d"
+
+  additional_windows_private_agent_os_user   = "${module.winagent.admin_username}"
+  additional_windows_private_agent_passwords = "${module.winagent.windows_passwords}"
+  additional_windows_private_agent_ips       = ["${concat(module.winagent.private_ips)}"]
+
   dcos_oauth_enabled = "false"
   dcos_security      = "strict"
 
   providers = {
-    azurerm = "azurerm"
+    azure = "azurerm"
   }
 
   dcos_version = "${var.dcos_version}"
 
   dcos_variant              = "${var.dcos_variant}"
   dcos_license_key_contents = "${var.dcos_license_key_contents}"
+
 }
 
+module "winagent" {
+  source  = "dcos-terraform/windows-instance/azurerm"
+
+  providers = {
+    azure = "azurerm"
+  }
+
+  location               = "${local.location}"
+  dcos_instance_os       = "${local.dcos_winagent_os}"
+  cluster_name           = "${local.cluster_name}"
+
+  hostname_format        = "winagt-%[1]d-%[2]s"
+
+  subnet_id              = "${module.dcos.infrastructure.subnet_id}"
+  resource_group_name    = "${module.dcos.infrastructure.resource_group_name}"
+  vm_size                = "${local.vm_size}"
+  admin_username         = "dcosadmin"
+  public_ssh_key         = "${var.ssh_public_key_file}"
+  num                    = "${var.num_windows_agents}"
+}
 variable "dcos_instance_os" {
   default = "centos_7.6"
 }
+
 
 variable "dcos_variant" {
   default = "ee"
@@ -47,7 +79,10 @@ variable "dcos_variant" {
 variable "dcos_license_key_contents" {}
 
 variable "dcos_version" {
-  default = "1.13.1"
+default = "1.13.1"
+}
+variable "ssh_public_key_file" {
+default = "./ssh-key.pub"
 }
 
 variable "num_masters" {
@@ -65,6 +100,11 @@ variable "num_public_agents" {
   default     = 1
 }
 
+variable "num_windows_agents" {
+  description = "Specify the amount of windows agents. These agents will provide your main resources"
+  default     = 1
+}
+
 output "masters-ips" {
   value = "${module.dcos.masters-ips}"
 }
@@ -75,4 +115,16 @@ output "cluster-address" {
 
 output "public-agents-loadbalancer" {
   value = "${module.dcos.public-agents-loadbalancer}"
+}
+output "public_ips" {
+  description = "Windows IP"
+  value = "${module.winagent.public_ips}"
+}
+output "windows_passwords" {
+  description = "Windows Password for user ${module.winagent.admin_username}"
+  value = "${module.winagent.windows_passwords}"
+}
+output "masters_dns_name" {
+  description = "This is the load balancer address to access the DC/OS UI"
+  value       = "${module.dcos.masters-loadbalancer}"
 }
