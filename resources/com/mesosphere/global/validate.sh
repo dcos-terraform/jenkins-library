b61819ea041b19c88ad8f8250eaab078c1b5fca2
Jan Ulferts <julferts@mesosphere.com>
2020-08-07 09:58:19 +0200

    fix: validate is different in 0.12+

--- resources/com/mesosphere/global/validate.sh
+++ resources/com/mesosphere/global/validate.sh
@@ -3,4 +3,9 @@ set +o xtrace
 set -o errexit
 
 terraform init -upgrade
-terraform validate -check-variables=false
+
+if terraform --version | grep v0.11; then
+  terraform validate -check-variables=false
+else
+  terraform validate
+fi
